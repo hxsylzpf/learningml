d2f011aa211679c4512b2dc6f6c1841d72d10faf
tacey <tacey_wong@163.com>
2014-09-23 19:00:28 +0800

    添加部分注释

---
+++ Ch02/createFirstPlot.py
@@ -0,0 +1,20 @@
+﻿#coding : tuf-8
+"""
+    绘制散点图
+"""
+#导入相应模块
+from numpy import *
+import kNN
+import matplotlib
+import matplotlib.pyplot as plt
+
+
+fig = plt.figure() #创建figure对象
+ax = fig.add_subplot(111)   #在图片对象实例上添加一个坐标轴其中的111是坐标轴比例
+datingDataMat,datingLabels = kNN.file2matrix('datingTestSet2.txt') #获得数据文件中的数据矩阵
+#ax.scatter(datingDataMat[:,1], datingDataMat[:,2]) #在坐标实例上绘制散点图
+ax.scatter(datingDataMat[:,0], datingDataMat[:,1], 15.0*array(datingLabels), 15.0*array(datingLabels))
+#ax.axis([-2,25,-0.2,2.0]) #坐标轴的区间
+plt.xlabel('Percentage of Time Spent Playing Video Games') #x轴标签
+plt.ylabel('Liters of Ice Cream Consumed Per Week')#y轴标签
+plt.show()#显示
--- Ch02/kNN.py
+++ Ch02/kNN.py
@@ -1,20 +1,30 @@
+#coding:utf-8
 '''
 Created on Sep 16, 2010
 kNN: k Nearest Neighbors
 
 Input:      inX: vector to compare to existing dataset (1xN)
+            inX:  和现存的数据集合进行比教的向量
             dataSet: size m data set of known vectors (NxM)
+                  
             labels: data set labels (1xM vector)
+                    数据集标签
             k: number of neighbors to use for comparison (should be an odd number)
+                    用于比对的近邻数
             
-Output:     the most popular class label
-
-@author: pbharrin
+Output:     the most popular class label 
+Output:     最受欢迎类标签
+@author: pbharrin（-<你丫的语文老师死的早，fuck your fucking code）
 '''
-from numpy import *
+
+#导入需要的模块
+
+from numpy import *#严重破坏命名空间
 import operator
 from os import listdir
 
+
+#分类函数
 def classify0(inX, dataSet, labels, k):
     dataSetSize = dataSet.shape[0]
     diffMat = tile(inX, (dataSetSize,1)) - dataSet
@@ -29,28 +39,33 @@ def classify0(inX, dataSet, labels, k):
     sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
 
+
+#创建数据集合
 def createDataSet():
-    group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
-    labels = ['A','A','B','B']
+    group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])#x
+    labels = ['A','A','B','B']#y
     return group, labels
+
 
 def file2matrix(filename):
     fr = open(filename)
-    numberOfLines = len(fr.readlines())         #get the number of lines in the file
-    returnMat = zeros((numberOfLines,3))        #prepare matrix to return
-    classLabelVector = []                       #prepare labels return   
-    fr = open(filename)
-    index = 0
-    for line in fr.readlines():
-        line = line.strip()
-        listFromLine = line.split('\t')
-        returnMat[index,:] = listFromLine[0:3]
-        classLabelVector.append(int(listFromLine[-1]))
+    numberOfLines = len(fr.readlines())         #get the number of lines in the file(获得文件的行数)
+    returnMat = zeros((numberOfLines,3))        #prepare matrix to return（准备要返回的矩阵【元素初始化为零的二维矩阵】）
+    classLabelVector = []                       #prepare labels return   （准备要返回的标签【列表】）
+    fr = open(filename)                         #这一语句不是没用，用于把文件指针重新移到文件开始处
+    index = 0                                   #索引初始化为0
+    for line in fr.readlines():                 #逐行读取文件
+        line = line.strip()                     #去除换行符
+        listFromLine = line.split('\t')         #通过\t对数据进行分割，返回列表
+        returnMat[index,:] = listFromLine[0:3]  #读取前三个数据（group），第四个是标签（label）【注意那个逗号】
+        classLabelVector.append(int(listFromLine[-1]))#追加标签
         index += 1
-    return returnMat,classLabelVector
-    
+    return returnMat,classLabelVector 
+
+
+#自动归一化，使得三类数据具有相同的权值   
 def autoNorm(dataSet):
-    minVals = dataSet.min(0)
+    minVals = dataSet.min(0) 
     maxVals = dataSet.max(0)
     ranges = maxVals - minVals
     normDataSet = zeros(shape(dataSet))
